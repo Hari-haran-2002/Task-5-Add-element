2b0d7199c9d9264132bc9154711c72652269ad9e
Hari-haran-2002 <jagahari2002@gmail.com>
2024-09-12 17:11:31 +0530

    Add files via upload

---
+++ addElement.js
@@ -0,0 +1,11 @@
+// Initialize an array
+let arr = [1, 2, 3, 4];
+
+// Element to add
+let newElement = 5;
+
+// Add the new element to the end of the array without using push
+arr[arr.length] = newElement;
+
+// Output the updated array
+console.log(arr);
